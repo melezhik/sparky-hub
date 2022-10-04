af3a210a638f1e4b069f207b9f4c3ee7d0d25f88
aleksei melezhik <aleksei_melezhik@epam.com>
2022-10-03 19:34:59 -0500

    add deploy action

--- .tom/tasks/deploy-hubcli/task.bash
+++ .tom/tasks/deploy-hubcli/task.bash
@@ -1,3 +1,3 @@
-curl -LJ "https://github.com/epam/hubctl/releases/latest/download/hub_$(uname -s)_$(uname -m).tar.gz" \
+curl -sLJ "https://github.com/epam/hubctl/releases/latest/download/hub_$(uname -s)_$(uname -m).tar.gz" \
 | tar xz -C /tmp
 mv -v /tmp/hub .
